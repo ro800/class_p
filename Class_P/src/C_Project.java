dc46cb26ff615137cff2012e7c86f457bbeaf541
ro800 <rm.jr.89@gmail.com>
2016-10-25 18:17:29 -0400

    Update C_Project.java

--- Class_P/src/C_Project.java
+++ Class_P/src/C_Project.java
@@ -7,36 +7,76 @@ public class C_Project {
 		
 		Scanner in = new Scanner(System.in);
 		ArrayList<Integer> list = new ArrayList<Integer>();
+		long startTime;
+		long endTime;
 		System.out.println("Enter the number of Random num: ");
 		int RanNum = in.nextInt();
 		System.out.println("Set range for random number: ");
 		int range = in.nextInt();
+		startTime = System.nanoTime();
 		for(int i = 0; i < RanNum; i++)
 		{
 			int ran = (int)(Math.random()*range);
 			list.add(ran);
 		}
-		//System.out.println(list);
-		//list = Oeder(list);
-		//System.out.println(list);
 		
-		//System.out.println(list);
-		//long startTime = System.nanoTime();
-		//QuickSort(list);
-		//long endTime = System.nanoTime();
-		//System.out.println("Took "+(endTime - startTime) + " ns"); 
 		System.out.println(list);
-		Sequential(list);
+		endTime = System.nanoTime();
+		System.out.println("Took "+(endTime - startTime) + " ns");
+		System.out.println((endTime - startTime)/1000000000);
+		
+		boolean flag = true;
+		
+			System.out.println("menu: ");
+			int menu = in.nextInt();
+			switch (menu){
+			case 1:
+				startTime = System.nanoTime();
+				QuickSort(list);
+				endTime = System.nanoTime();
+				System.out.println("Took "+(endTime - startTime) + " ns");
+				System.out.println((endTime - startTime)/1000000000);
+				break;
+			case 2: //Not Valid part!!!!!!
+				startTime = System.nanoTime();
+				list = Oeder(list);
+				System.out.println(list);
+				endTime = System.nanoTime();
+				System.out.println("Took "+(endTime - startTime) + " ns");
+				System.out.println((endTime - startTime)/1000000000);
+				break;
+			case 3:
+				startTime = System.nanoTime();
+				bubble(list);
+				endTime = System.nanoTime();
+				System.out.println("Took "+(endTime - startTime) + " ns");
+				System.out.println((endTime - startTime)/1000000000);
+				break;
+			case 4:
+				System.out.println("Enter number you want to find: ");
+				int lookFor = in.nextInt();
+				startTime = System.nanoTime();
+				Sequential(list, lookFor);
+				endTime = System.nanoTime();
+				System.out.println("Took "+(endTime - startTime) + " ns");
+				System.out.println((endTime - startTime)/1000000000);
+				break;
+			case 5:
+				break;
+				
+			}
 		
 		
 		
+		
+			
 	}
+	
 	public static ArrayList<Integer> Oeder(ArrayList<Integer> list){
 		Collections.sort(list);
 		return list;
 	}
 	public static ArrayList<Integer> QuickSort(ArrayList<Integer> list){
-		
 		if(list.size() <= 1){
 			return list;
 		}
@@ -60,27 +100,19 @@ public class C_Project {
 		return concatenate(QuickSort(less), pivot, QuickSort(greater));
 	}
 	public static ArrayList<Integer> concatenate(ArrayList<Integer> less, int pivot, ArrayList<Integer> greater){
-			
 			ArrayList<Integer> list = new ArrayList<Integer>();
-			
 			for (int i = 0; i < less.size(); i++) {
 				list.add(less.get(i));
 			}
-			
 			list.add(pivot);
-			
 			for (int i = 0; i < greater.size(); i++) {
 				list.add(greater.get(i));
 			}
 			System.out.println(list);
 			return list;
 		}
-		
-	
-	public static void Sequential(ArrayList<Integer> list){
-		Scanner enter = new Scanner(System.in);
-		System.out.println("Enter number you want to find: ");
-		int look = enter.nextInt();
+	public static void Sequential(ArrayList<Integer> list, int lookFor){
+		int look = lookFor;
 		boolean bool = false;
 		for(int i = 0; i < list.size(); i++){
 			if(look == list.get(i)){
@@ -89,9 +121,27 @@ public class C_Project {
 			}
 		}
 		if(bool==false){
-			System.out.println("Number " + look + " is not located in the array.");
+			System.out.println("Number " + look + " is NOT located in the array.");
 		}
 	}
+	
+	public static void bubble(ArrayList<Integer> list){
+		boolean x = true;
+		while(x){
+			x=false;
+			for(int i = 0; i < list.size()-1; i++){
+				if(list.get(i) > list.get(i+1)){
+					int big = list.get(i);
+					int small = list.get(i+1);
+					list.set(i+1, big);
+					list.set(i, small);
+					x=true;
+				}
+			}
+		}
+		System.out.println(list);
+	}
+	
 	
 	
 	
